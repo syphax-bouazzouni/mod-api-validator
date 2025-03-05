7e9bb425e62afd80bbf9d4ca7c4f31a99e780f99
Syphax <gs_bouazzouni@esi.dz>
2025-03-05 05:03:24 +0100

    add react query

---
+++ src/lib/ReactQueryProvider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query";
+import {useState} from "react";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const ReactQueryProvider = ({children}: { children: React.ReactNode }) => {
+        const [queryClient] = useState(() => new QueryClient({
+            defaultOptions: {
+                queries: {
+                    retry: 0,
+                }
+            }
+        }));
+
+        return (
+            <QueryClientProvider client={queryClient}>
+                {children}
+                <ReactQueryDevtools initialIsOpen={false}/>
+            </QueryClientProvider>
+        );
+    }
+;
+
+export default ReactQueryProvider;
