c64b10d1b67ad3fa48dc96ca5f6f38ac5a7740d0
Syphax <gs_bouazzouni@esi.dz>
2025-04-04 09:43:59 +0200

    add option to have specific params for tested baseUrl

--- src/app/validator/page.tsx
+++ src/app/validator/page.tsx
@@ -8,10 +8,19 @@ import {Label} from "@/components/ui/label";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import {ModAPIValidatorResults} from "@/app/validator/components/ModAPIValidatorResults";
 import {useModApiFetcher} from "@/app/explore/components/openapi-explorer";
+import {getEnvironmentData} from "node:worker_threads";
+
+interface Option {
+    name?: string;
+    baseURL: string;
+    apikey?: string;
+    params?: string;
+}
 
 export default function ModAPIValidator() {
     const [baseURL, setBaseURL] = useState('');
     const [apikey, setApikey] = useState('');
+    const [params, setParams] = useState('');
     const [fetchBaseURL, setFetchBaseURL] = useState("");
     const [option, setOption] = useState({});
     const {
@@ -24,10 +33,20 @@ export default function ModAPIValidator() {
         updateYamlUrl,
     } = useModApiFetcher()
 
-    const options = [
-        {name: 'OntoPortal staging', baseURL: 'https://data.stageportal.lirmm.fr', apikey: '1de0a270-29c5-4dda-b043-7c3580628cd5'},
+    const options: Option[] = [
+        {
+            name: 'OntoPortal staging',
+            baseURL: 'https://data.stageportal.lirmm.fr',
+            apikey: '1de0a270-29c5-4dda-b043-7c3580628cd5'
+        },
         {name: 'The NERC Vocabulary Server (NVS)', baseURL: 'https://vocab.nerc.ac.uk'},
-        {name: 'TS-NFDI API Gateway', baseURL: 'https://ts4nfdi-api-gateway.prod.km.k8s.zbmed.de/api-gateway'},
+        {name: 'TIB (OLS)', baseURL: 'https://service.tib.eu/terminology/mod/api'},
+        {
+            name: 'TS-NFDI API Gateway',
+            // baseURL:'http://localhost:8080/api-gateway',
+            baseURL: 'https://ts4nfdi-api-gateway.prod.km.k8s.zbmed.de/api-gateway',
+            params: "format=jsonld&targetDbSchema=mod&showResponseConfiguration=false&database=ontoportal"
+        },
     ]
 
     if (isLoading) {
@@ -39,20 +58,22 @@ export default function ModAPIValidator() {
             from {yamlUrl}: {error?.message}</div>
     }
 
-    const updateBaseUrl = (baseUrl: string, apikey: string | undefined) => {
+    const updateBaseUrl = (option: Option) => {
+        const {baseURL, apikey, params} = option
+
         setFetchBaseURL('')
         setApikey(apikey || '')
-        setBaseURL(baseUrl)
+        setBaseURL(baseURL)
+        setParams(params || '')
     }
 
-    const updateOption = (option: any) => {
+    const updateOption = (option: Option) => {
         setOption(option)
-        updateBaseUrl(option.baseURL, option.apikey)
+        updateBaseUrl(option)
     }
 
 
-    return (<>
-            <Card className="container mx-auto my-2">
+    return <Card className="container mx-auto my-2">
                 <CardHeader>
                     <CardTitle>MOD API validator</CardTitle>
                 </CardHeader>
@@ -92,10 +113,10 @@ export default function ModAPIValidator() {
                                         <Input
                                             placeholder="Enter Base path URL of you API to validate"
                                             value={baseURL}
-                                            onChange={(e) => updateBaseUrl(e.target.value, apikey)}
+                                            onChange={(e) => updateBaseUrl({baseURL: e.target.value,apikey: apikey, params: params})}
                                         />
                                     </div>
-                                    {option.apikey &&  <div>
+                                    {option.apikey && <div>
                                         <Label>API key</Label>
                                         <Input
                                             placeholder="Enter API key"
@@ -103,20 +124,25 @@ export default function ModAPIValidator() {
                                             onChange={(e) => setApikey(e.target.value)}
                                             className="w-1/2"/>
                                     </div>}
-                                </div>
-                                }
+                                    {option.params && <div>
+                                        <Label>Additional parameters</Label>
+                                        <Input
+                                            placeholder="Enter additional parameters"
+                                            value={params}
+                                            onChange={(e) => setParams(e.target.value)}
+                                            className="w-1/2"/>
+                                    </div>}
+                                </div>}
                             </div>
                             <Button className={'w-full'} onClick={() => setFetchBaseURL(baseURL)}
-                                    disabled={Object.keys(option).length==0 || !baseURL || isLoading}>
+                                    disabled={Object.keys(option).length == 0 || !baseURL || isLoading}>
                                 Validate
                             </Button>
                         </div>
                         <div>
-                            <ModAPIValidatorResults model={modAPI} baseUrl={fetchBaseURL} apikey={apikey}/>
+                            <ModAPIValidatorResults model={modAPI} baseUrl={fetchBaseURL} apikey={apikey} params={params}/>
                         </div>
                     </div>
                 </CardContent>
             </Card>
-        </>
-    )
 }
